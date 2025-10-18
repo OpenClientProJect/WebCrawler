18d6c59dc1e42facbe1385c11a1aa2471f522993
admin <811489637@qq.com>
2025-10-18 17:21:44 +0800

    thup2

--- FBver/FB_win.py
+++ FBver/FB_win.py
@@ -3,6 +3,6 @@ import asyncio
 from FB_middle import main
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        asyncio.run(main("272275"))
     except KeyboardInterrupt:
         print(111)
